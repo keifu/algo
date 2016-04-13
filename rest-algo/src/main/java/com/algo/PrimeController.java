214efd259355e3c3334124a783f9c5566f3b3d39
keifu <keithjim2002@hotmail.com>
2016-04-13 21:53:01 +0100

    clean up

--- rest-algo/src/main/java/com/algo/PrimeController.java
+++ rest-algo/src/main/java/com/algo/PrimeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping("/prime")
 public class PrimeController {
 
